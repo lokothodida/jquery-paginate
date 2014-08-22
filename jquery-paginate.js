144171cf131619c35bd234e9f3beab8fc5ce6aaf
Lawrence Paul Okoth-Odida <lawrence.okothodida@gmail.com>
2014-08-22 16:07:08 +0100

    Check type signature

--- jquery-paginate.js
+++ jquery-paginate.js
@@ -3,7 +3,17 @@
  */;
 (function($, window, document) {
 $.fn.paginate = function(parameters) {
-  //...
+  // method being called (after plugin has initialized)
+  if (typeof parameters === 'string') {
+    if (parameters.toLowerCase() == 'destroy') {
+      destroy();
+    } else {
+      throw 'Method "' + parameters + '" does not exist in jQuery Paginate';
+    }
+    return this.each(function() {});
+  }
+
+  // initializing the plugin
 
   return this.each(function() {
     // ...
